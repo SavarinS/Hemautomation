18beed2011c77a1d2efff12f2a66a0b40e5555e6
Savarin Sitisarn <savarin.sitisarn@gmail.com>
2019-03-20 10:33:30 +0100

    Content styling

--- src/app/home4/home4.component.ts
+++ src/app/home4/home4.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -7,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home4.component.scss']
 })
 export class Home4Component implements OnInit {
+  title = 'Hus 4';
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/homes/3';
+  object: Object;
+  constructor(private http: HttpClient) { 
+    console.log('Hello from contructor home4');
+    this.getData();
+    this.getHomes();
+  }
+
+  getData(){
+    console.log('Hello from getData function home4');
+    return this.http.get(this.apiUrl)
+  }
+
+  getHomes(){
+    console.log('Hello from getHomes function home4');
+    this.getData().subscribe(object => {
+      console.log(object);
+      this.object = object
+    })
+  }
 
   ngOnInit() {
   }
